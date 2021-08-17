849fc9477180a444b534a83595dd0199e9b88d98
azlan <mhdazlanabaziz@gmai.com>
2021-08-17 15:48:42 +0800

    basic get setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # theLibraryApp
+##https://stackoverflow.com/questions/68775869/support-for-password-authentication-was-removed-please-use-a-personal-access-to
+## npm i -D prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node
--- app.js
+++ app.js
@@ -6,16 +6,28 @@ const path = require('path');
 
 const app = express();
 
+const bookRouter = require('./src/routes/bookRoutes');
+
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.get('/', function (req, res) {
-  res.send('Hello from my library app');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/book', bookRouter);
+app.get('/', (req, res) => {
+  res.render('index', {
+    nav: [
+      { link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' },
+    ],
+    title: 'Library',
+  });
 });
 
 app.get('/test', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  debug(`listening at port ${chalk.green('3000')}`);
 });
