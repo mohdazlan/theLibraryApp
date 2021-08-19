cb314855b6d255ffd54333a6985d05b014354fbc
azlan <mhdazlanabaziz@gmai.com>
2021-08-20 04:54:37 +0800

    mbasic passport setup created a user

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug')('app');
 const morgan = require('morgan');
+const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
 const app = express();
-
-app.use(morgan('combined'));
-app.use(bodyParser.json());
+app.use(morgan('dev'));
+// To parse URL encoded data
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({ secret: 'library' }));
+
+// To parse json data
+app.use(bodyParser.json());
+
 require('./src/config/passport')(app);
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'library',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
@@ -26,6 +33,8 @@ const nav = [
   { link: '/books', title: 'Book' },
   { link: '/authors', title: 'Author' },
 ];
+
+const debug = require('debug')('app');
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
@@ -48,7 +57,10 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+app.get('/fuck', (req, res) => {
+  res.send('WTF');
+});
 
-app.listen(3000, () => {
-  debug(`listening at port ${chalk.green('3000')}`);
+app.listen(5000, () => {
+  debug(`listening at port ${chalk.green('5000')}`);
 });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const debug = require('debug')('app:adminRoutes');
+const debug = require('debug')('app:authRoutes');
+
 const authRouter = express.Router();
 
 function router() {
   authRouter.route('/signUp').post((req, res) => {
-    debug(req.body);
-    //create user
-    req.login(req.body, () => {
-      res.redirect('/auth/profile');
-    });
+    const { username, password } = req.body;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function addUser() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Hey we are connected to the server');
+
+        const db = client.db(dbName);
+
+        const col = db.collection('users');
+        const user = { username, password };
+        const results = await col.insertOne(user);
+        debug(results);
+        req.login(results.ops[0], () => {
+          res.redirect('/auth/profile');
+        });
+      } catch (err) {
+        debug(err);
+      }
+    })();
   });
   authRouter.route('/profile').get((req, res) => {
     res.json(req.user);
