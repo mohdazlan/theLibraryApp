c7313332b5b285a7ba7ac20d52b6bc90157d1fd9
azlan <mhdazlanabaziz@gmai.com>
2021-08-19 09:37:08 +0800

    basic setup of passport

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug');
+const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
-const bookRouter = require('./src/routes/bookRoutes');
-
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+require('./src/config/passport')(app);
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' },
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
+
 app.use('/book', bookRouter);
+app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
 app.get('/', (req, res) => {
   res.render('index', {
     nav: [
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
+const authRouter = express.Router();
+
+function router() {
+  authRouter.route('/signUp').post((req, res) => {
+    debug(req.body);
+    //create user
+    req.login(req.body, () => {
+      res.redirect('/auth/profile');
+    });
+  });
+  authRouter.route('/profile').get((req, res) => {
+    res.json(req.user);
+  });
+  return authRouter;
+}
+
+module.exports = router;
